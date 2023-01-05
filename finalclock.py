3cce25427e1ee14b4a9b81287b59fee9d97eea6e
psypad <112978372+SylanPadmakumar@users.noreply.github.com>
2023-01-06 01:40:04 +0530

    Update finalclock.py

--- finalclock.py
+++ finalclock.py
@@ -1,38 +1,42 @@
 from tkinter import *
 import time
-#import os
-
-# def update():
-#    lab['text'] = randint(0,1000)
-#    root.after(1000,update) 
+import playsound
 
 root = Tk()
+root.geometry("310x300")
 
-TimeLabel = Label(root)
+textlabel = Label(root, text= "set alarm: ", font=('Helvetica', 14))
+textlabel.place(relx=0.34, rely=0.1, anchor=E)
 
-TimeLabel.pack()
+e = Entry(root, width = 45,borderwidth = 5)
+e.place(relx=0.5, rely=0.2, anchor=CENTER)
 
+TimeLabel = Label(root,font=('Helvetica bold', 26))
+TimeLabel.place(relx=0.5, rely=0.5, anchor=CENTER)
+
+
+
+def button_click():
+    global h
+    h = e.get()
+    e.delete(0,END)
+    
+# button_1 = Button(root, text = "enter", padx = 40, pady = 20, command =button_click())
+# button_1.place(relx=0.5, rely=0.5, anchor=CENTER)
 
 def update():
-   TimeLabel['text'] = b
-   root.after(1000,update)
-
-a = time.localtime()
-h = a[3]
-m = a[4]
-s = a[5]
-b = h,":",m,":",s
+    a = time.localtime()
+    h = a[3]
+    m = a[4]
+    s = a[5]
+    global b
+    b = h,":",m,":",s
+    TimeLabel['text'] = b
+    root.after(1000,update)
 
 update()
 
-root.mainloop()
+# if b == h:
+#     playsound('alarm.wav')
 
-# while True:
-#     a = time.localtime()
-#     h = a[3]
-#     m = a[4]
-#     s = a[5]
-#     b = h,":",m,":",s
-#     update()
-#     #time.sleep(1)
-#     #os.system('cls')
+root.mainloop()

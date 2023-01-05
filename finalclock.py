82f0beb77e3ab7e3c38784c8b9737c4b9fa098b2
psypad <112978372+SylanPadmakumar@users.noreply.github.com>
2023-01-06 00:53:02 +0530

    Update finalclock.py

--- finalclock.py
+++ finalclock.py
@@ -1,10 +1,5 @@
 from tkinter import *
 import time
-#import os
-
-# def update():
-#    lab['text'] = randint(0,1000)
-#    root.after(1000,update) 
 
 root = Tk()
 
@@ -14,25 +9,17 @@ TimeLabel.pack()
 
 
 def update():
-   TimeLabel['text'] = b
-   root.after(1000,update)
+    a = time.localtime()
+    h = a[3]
+    m = a[4]
+    s = a[5]
+    b = h,":",m,":",s
+    TimeLabel['text'] = b
 
-a = time.localtime()
-h = a[3]
-m = a[4]
-s = a[5]
-b = h,":",m,":",s
+    root.after(1000,update)
+
 
 update()
 
 root.mainloop()
 
-# while True:
-#     a = time.localtime()
-#     h = a[3]
-#     m = a[4]
-#     s = a[5]
-#     b = h,":",m,":",s
-#     update()
-#     #time.sleep(1)
-#     #os.system('cls')

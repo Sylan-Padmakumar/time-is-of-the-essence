6ac018d0f7c9abd2e585e853135a8c0fff9f8a91
psypad <112978372+SylanPadmakumar@users.noreply.github.com>
2023-01-07 02:23:28 +0530

    Update finalclock.py

--- finalclock.py
+++ finalclock.py
@@ -1,38 +1,108 @@
 from tkinter import *
+from tkinter import ttk
+from winsound import PlaySound, SND_FILENAME, SND_LOOP, SND_ASYNC
 import time
-#import os
+import os
 
-# def update():
-#    lab['text'] = randint(0,1000)
-#    root.after(1000,update) 
+#functions
+
+class button():
+    def select():
+        my_notebook.select(0)
+
+    def select2():
+        my_notebook.select(1)
+        
+    def select3():
+        my_notebook.select(2)
+        
+def snooze():
+    global i
+    i = 1
+    print(i)
+    
+
+        
+class clock(button):
+    def Time():
+        a = time.localtime()
+        h = a[3]
+        m = a[4]
+        s = a[5]
+        global b
+        b = h,":",m,":",s
+        TimeLabel['text'] = b
+        root.after(1000,clock.Time)
+    
+    def alarm():
+        global i
+        user_alarm_time = [alarm_hour.get(),alarm_min.get()]
+        if user_alarm_time[0] == str(b[0]) and user_alarm_time[1] == str(b[2]):
+             PlaySound('alarm.wav',SND_FILENAME|SND_LOOP|SND_ASYNC)
+             print(i)
+        elif i == 1:
+            PlaySound(None, SND_FILENAME)
+
+
+
+#PROGRAM BODY
+
+i = 0
+
+#GUI
+
 
 root = Tk()
+root.geometry("310x300")
 
-TimeLabel = Label(root)
+#FRAMES
+my_notebook = ttk.Notebook(root)
+my_notebook.pack()
 
-TimeLabel.pack()
+ttk.Style().layout('TNotebook.Tab', [])
+
+my_frame1 = Frame(my_notebook, width = 310, height = 300)
+my_frame2 = Frame(my_notebook, width = 310, height = 300)
+my_frame3 = Frame(my_notebook, width = 310, height = 300)
+
+my_notebook.add(my_frame1)
+my_notebook.add(my_frame2)
+my_notebook.add(my_frame3)
+
+my_frame2.grid_propagate(False)
+
+my_button = Button(my_frame2, text = "home", command = button.select,pady = 15).grid()
+my_button = Button(my_frame3, text = "home", command = button.select,pady = 15).grid()
+my_button_to_alarm = Button(my_frame1, text = "alarm", command = button.select2,pady = 15).grid()
+my_button3_to_timer = Button(my_frame1, text = "timer", command = button.select3,pady = 15).grid()
+my_button_snooze = Button(my_frame2, text = "snooze", command = snooze ,pady = 50).grid()
+
+#CLOCK GUI
+textlabel = Label(my_frame2, text= "set alarm: ", font=('Helvetica', 14))
+textlabel.place(relx=0.34, rely=0.1, anchor=E)
+
+alarm_hour = Entry(my_frame2, width = 15,borderwidth = 5,) 
+alarm_hour.place(relx=0.3, rely=0.3, anchor=CENTER)
+
+alarm_min = Entry(my_frame2, width = 15,borderwidth = 5,)
+alarm_min.place(relx=0.7, rely=0.3, anchor=CENTER)
+
+TimeLabel = Label(my_frame1,font=('Helvetica bold', 26))
+TimeLabel.place(relx=0.5, rely=0.7, anchor=CENTER)
+
+button_1 = Button(my_frame2, text = "enter", padx = 8, pady = 4,command = clock.alarm)
+button_1.place(relx=0.8, rely=0.4, anchor=CENTER)
 
 
-def update():
-   TimeLabel['text'] = b
-   root.after(1000,update)
+#END
 
-a = time.localtime()
-h = a[3]
-m = a[4]
-s = a[5]
-b = h,":",m,":",s
-
-update()
+clock.Time()
+print(b)
+clock.alarm()
 
 root.mainloop()
 
-# while True:
-#     a = time.localtime()
-#     h = a[3]
-#     m = a[4]
-#     s = a[5]
-#     b = h,":",m,":",s
-#     update()
+#fin
+
 #     #time.sleep(1)
 #     #os.system('cls')
